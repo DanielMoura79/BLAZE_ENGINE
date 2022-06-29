61f757462e0ecdb8ceb9b2cc505bc421db837141
dipi evil <dipi.evil@gmail.com>
2022-06-29 18:27:39 -0300

    Add missing res directory

---
+++ res/gfx.h
@@ -0,0 +1,34 @@
+#ifndef _RES_GFX_H_
+#define _RES_GFX_H_
+
+extern const Image bga_credits;
+extern const Image bgb_credits;
+extern const Image bgb_logo;
+extern const Image bgb_select;
+extern const Image bga_overlayer;
+extern const Image bg_null;
+extern const Image map0101;
+extern const Image map0102;
+extern const Image map0103;
+extern const Image map0104;
+extern const Image map0105;
+extern const Image map0106;
+extern const Image map0107;
+extern const Image map0108;
+extern const Image map0109;
+extern const Image map0110;
+extern const Image map0111;
+extern const Image map0112;
+extern const Image map0113;
+extern const Image map0114;
+extern const Image map0115;
+extern const Image map0201;
+extern const Image map0202;
+extern const Image map0203;
+extern const Image map0204;
+extern const Image map0205;
+extern const Image map0206;
+extern const Image map0207;
+extern const Image map0208;
+
+#endif // _RES_GFX_H_
